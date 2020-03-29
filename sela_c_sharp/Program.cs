d3e61bdee718dd15143b4035675e1325285bfeec
yanivn2000 <yanivn2000@gmail.com>
2020-03-29 11:06:36 +0300

    base

---
+++ sela_c_sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sela_c_sharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
