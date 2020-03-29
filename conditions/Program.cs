6bc90c41955766895dbf0bbf0419b824c01a87ce
yanivn2000 <yanivn2000@gmail.com>
2020-03-29 11:20:30 +0300

    base 4

---
+++ conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conditions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
