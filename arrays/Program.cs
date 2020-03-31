68b94edc6c6b8c1baf052f6c24ea665c543c9d6f
yanivn2000 <yanivn2000@gmail.com>
2020-03-31 15:42:12 +0300

    new

---
+++ arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace arrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //color array
+            ConsoleColor[] colorArr = new ConsoleColor[]{
+                ConsoleColor.Black, ConsoleColor.DarkBlue,
+                ConsoleColor.DarkGreen,
+                ConsoleColor.DarkCyan,
+                ConsoleColor.DarkRed,
+                ConsoleColor.DarkMagenta,
+                ConsoleColor.DarkYellow,
+                ConsoleColor.Gray,
+                ConsoleColor.DarkGray,
+                ConsoleColor.Blue,
+                ConsoleColor.Green,
+                ConsoleColor.Cyan,
+                ConsoleColor.Red,
+                ConsoleColor.Magenta,
+                ConsoleColor.Yellow,
+                ConsoleColor.White,
+                ConsoleColor.Black,
+                ConsoleColor.DarkBlue,
+                ConsoleColor.DarkGreen,
+                ConsoleColor.DarkCyan,
+                ConsoleColor.DarkRed,
+                ConsoleColor.DarkMagenta,
+                ConsoleColor.DarkYellow,
+                ConsoleColor.Gray,
+                ConsoleColor.DarkGray,
+                ConsoleColor.Blue,
+                ConsoleColor.Green,
+                ConsoleColor.Cyan,
+                ConsoleColor.Red,
+                ConsoleColor.Magenta,
+                ConsoleColor.Yellow,
+                ConsoleColor.White
+            };
+
+            Random rand = new Random();
+            int max = colorArr.Length;
+            while (true)
+            {
+                int color_picked;
+                string input;
+                do
+                {
+                    Console.WriteLine($"Please enter a number between 0 to {max - 1}: ");
+                    input = Console.ReadLine();
+                } while (!int.TryParse(input, out color_picked) || color_picked < 1 || color_picked > max);
+                Console.ForegroundColor = colorArr[color_picked];
+            }
+
+        }
+    }
+}
---
+++ random/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace conditions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello raandom!");
+            Random rand = new Random();
+            int lines = 100000;
+            for (int i = 0; i < lines; i++)
+            {
+                int value = rand.Next(1,50);
+                for (int j = 0; j < value; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+    }
+}
