4a804410929c33e5ed1120fa2495e32eb105e151
yanivn2000 <yanivn2000@gmail.com>
2020-06-02 15:50:55 +0300

    this is the end (almost)

--- Sets-50150/Program.cs
+++ Sets-50150/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sets50150
 {
@@ -34,18 +35,58 @@ namespace Sets50150
             Sets set5 = new Sets(1, 2, 3, 4, 5, 6, 7, 8, 9);
             //int[] array = { 1, 2, 3, 55 };
             Sets set6 = new Sets(3, 5, 7, 9);
-            if (set5.Subset(set6)) Console.WriteLine("Set6 is a subset of Set5");
-            else Console.WriteLine("Set6 is not a subset of Set5");
-            if (set5.Subset(set2)) Console.WriteLine("Set2 is a subset of Set5");
-            else Console.WriteLine("Set2 is not a subset of Set5");
+            if (set5.Subset(set6)) Console.WriteLine("Set6 is a subset of Set5 - TRUE");
+            else Console.WriteLine("Set6 is not a subset of Set5 - FALSE");
+            if (set5.Subset(set2)) Console.WriteLine("Set2 is a subset of Set5 - FALSE");
+            else Console.WriteLine("Set2 is not a subset of Set5 - TRUE");
+
+
+            Console.WriteLine("Check if 5 is a member of set5:");
+            if(set5.isMember(5))
+                Console.WriteLine("5 is a member of set5 - TRUE");
+            else
+                Console.WriteLine("5 is NOT a member of set5 - FALSE");
+
+            Console.WriteLine("Check if 50 is a member of set5:");
+            if (set5.isMember(50))
+                Console.WriteLine("50 is a member of set5 - FALSE");
+            else
+                Console.WriteLine("50 is NOT a member of set5 - TRUE");
+
+            Console.WriteLine("Insert 50 to set5:");
+            set5.Insert(50);
+            if (set5.isMember(50))
+                Console.WriteLine("50 is a member of set5 - TRUE");
+            else
+                Console.WriteLine("50 is NOT a member of set5 - FALSE");
+
+            Console.WriteLine("Delete 50 from set5:");
+            set5.Delete(50);
+            if (set5.isMember(50))
+                Console.WriteLine("50 is a member of set5 - FALSE");
+            else
+                Console.WriteLine("50 is NOT a member of set5 - TRUE");
+
+
+            /*
+             * Check Equals
+             */
+            Sets set7 = new Sets(3, 5, 7, 9);
+            Sets set8 = new Sets(3, 5, 7, 9);
+            Sets set9 = new Sets(3, 7, 9);
+            if (set7.Equals(set8)) Console.WriteLine("set 7 and set 8 are equal - TRUE");
+            else Console.WriteLine("set 7 and set 8 are NOT equal - FALSE");
+
+            if (set7.Equals(set9)) Console.WriteLine("set 7 and set 9 are equal - FALSE");
+            else Console.WriteLine("set 7 and set 9 are NOT equal - TRUE");
+
         }
     }
 
     class Sets
     {
         bool[] _set;
-        const int _size_set = 1001;
-        string _name;
+        static int _size_set = 1001;
         //Empty constructor
         public Sets()
         {
@@ -55,6 +96,7 @@ namespace Sets50150
                 _set[i] = false;
             }
         }
+        //constructor with params
         public Sets(params int [] values) : this()
         {
             foreach (var value in values)
@@ -77,6 +119,11 @@ namespace Sets50150
             }
             str += $"Number of elements: {counter}";
             return str;
+        }
+        //check if number is in range of the set
+        private bool isInRange(int number)
+        {
+            return (number < _size_set && number >= 0);
         }
         //union – a union operation that receives a set and unites it with the original set.i.e.
         //the set the operation was being done on will represent the union.For Example:
@@ -113,6 +160,40 @@ namespace Sets50150
                     return false;
             }
             return true;
+        }
+
+        //isMember – an operation that receives a member and checks whether it belongs to the set.
+        public bool isMember(int number)
+        {
+            if (!isInRange(number)) return false;
+            return _set[number];
+        }
+        //insert – an operation that receives a member and inserts it to the set.
+        public bool Insert(int number)
+        {
+            if (!isInRange(number)) return false;
+            _set[number] = true;
+            return true;
+        }
+        //Delete – an operation that receives a member and deletes it from the set
+        public bool Delete(int number)
+        {
+            if (!isInRange(number)) return false;
+            _set[number] = false;
+            return true;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+            Sets set2 = (Sets)obj;
+            for (int i = 0; i < _size_set; i++)
+            {
+                if (_set[i] != set2._set[i]) return false;
+            }
+            return true;
+
         }
     }
 }
