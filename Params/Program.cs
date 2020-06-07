3a194062b4625717a4c986dd275a6293df8e0144
yanivn2000 <yanivn2000@gmail.com>
2020-06-07 11:22:20 +0300

    Params

---
+++ Params/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Params
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Params");
+            int[] array = new int[] { 3, 5, 7, 9, 11 };
+            PrintArray("Send Array", array);
+            PrintArray("Send List", 2, 4, 6, 8, 10);
+        }
+        public static void PrintArray(string str, params int [] array)
+        {
+            Console.WriteLine($"{str}");
+
+            foreach (var item in array)
+            {
+                Console.WriteLine($"item: {item}");
+            }
+        }
+    }
+
+}
