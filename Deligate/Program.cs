95b6202fd2f67dac639e3e103f69eda1642490d6
yanivn2000 <yanivn2000@gmail.com>
2020-06-30 10:04:41 +0300

    deligate

--- Deligate/Program.cs
+++ Deligate/Program.cs
@@ -4,7 +4,7 @@ namespace Deligate
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)  
         {
             SomeClass obj = new SomeClass();
             obj.deligated1 += OnCallBack1;
