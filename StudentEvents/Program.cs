7d4327b0b1f1bd59eee6e6b54c9f2c4a38861413
yanivn2000 <yanivn2000@gmail.com>
2020-07-01 17:47:07 +0300

    b2

--- StudentEvents/Program.cs
+++ StudentEvents/Program.cs
@@ -17,45 +17,59 @@ namespace StudentEvents
         StudentQueue studentQueue;
         public Manager()
         {
-            studentQueue = new StudentQueue(70);
+            studentQueue = new StudentQueue(70, 92);
             Random rand = new Random();
             for (int i = 0; i < 100; i++)
             {
                 Student stud = new Student(rand.Next(100000, 200000), rand.Next(50, 100));
-                studentQueue.AddStudent(stud);
+                try
+                {
+                    studentQueue.AddStudent(stud);
+                }
+                catch(QueueLimitationException e)
+                {
+                    Console.WriteLine($"Exception! {e.Message}");
+                    studentQueue.RemoveByGrade();
+                }
             }
         }
         public void PrintAll()
         {
+            Console.WriteLine("Queue:");
             studentQueue.printQueue();
         }
     }
     class StudentQueue
     {
         int MinGrade;
+        int MaxStudents;
         List<Student> _students;
-        public StudentQueue(int minimumGrade)
+        public StudentQueue(int minimumGrade, int maxStudents)
         {
             MinGrade = minimumGrade;
+            MaxStudents = maxStudents;
             _students = new List<Student>();
 
         }
         public void RemoveByGrade()
         {
-            foreach (Student stud in _students)
+            _students.RemoveAll((x) => x.grade < MinGrade);             /*
+            for (int i = _students.Count - 1; i >= 0; i--)
             {
-                if (stud.grade < MinGrade)
-                {
-                    _students.Remove(stud);
-                }
-            }
+                if (_students[i].grade < MinGrade)
+                    _students.RemoveAt(i);
+            }*/
         }
         public void AddStudent(Student student)
         {
             _students.Add(student);
+            if(_students.Count > 92){
+                throw new QueueLimitationException($"Queue exceeded limitation: {MaxStudents}");
+            }
         }
         public void printQueue()
         {
+            Console.WriteLine($"Total number of students: {_students.Count}");
             foreach (Student stud in _students)
             {
                 Console.WriteLine(stud);
@@ -78,5 +92,19 @@ namespace StudentEvents
         }
     }
 
-
+    public class QueueLimitationException : Exception
+    {
+        public QueueLimitationException()
+            : base("this is an empty exception")
+        {
+        }
+        public QueueLimitationException(string message)
+            : base(message)
+        {
+        }
+        public QueueLimitationException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
 }
