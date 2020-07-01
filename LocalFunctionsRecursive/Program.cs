dd8ab2672484a21c920e6d8b98be07aa11eac226
yanivn2000 <yanivn2000@gmail.com>
2020-07-01 09:27:00 +0300

    Local Function Palindrom Recusrive

---
+++ LocalFunctionsRecursive/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LocalFunctionsRecursive
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = "Hello";
+            Console.WriteLine($"String {str} is Palindrom: {IsWordPalindrome(str)}");
+            str = "ABNNBA";
+            Console.WriteLine($"String {str} is Palindrom: {IsWordPalindrome(str)}");
+            str = "ABNNBA1";
+            Console.WriteLine($"String {str} is Palindrom: {IsWordPalindrome(str)}");
+            str = "ABxxxxxBA";
+            Console.WriteLine($"String {str} is Palindrom: {IsWordPalindrome(str)}");
+        }
+
+        static bool IsWordPalindrome(string word)
+        {
+            if (word == null)
+                throw new ArgumentNullException(nameof(word));
+
+            if (word.Length < 2)
+                return true;
+
+            return IsPalidrome(0, word.Length - 1);
+
+            bool IsPalidrome(int lo, int hi)
+            {
+                if (lo >= hi)
+                    return true;
+
+                if (char.ToUpperInvariant(word[lo]) !=
+                    char.ToUpperInvariant(word[hi]))
+                    return false;
+
+                return IsPalidrome(lo + 1, hi - 1);
+            }
+        }
+    }
+}
+
+
