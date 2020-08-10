9aa529da3852b9b1ff5f2718c45df6b2a1c03e8c
yanivn2000 <yanivn2000@gmail.com>
2020-08-10 14:30:58 +0300

    has 3

--- Hash_Chaining/Program.cs
+++ Hash_Chaining/Program.cs
@@ -7,12 +7,31 @@ namespace Hash_Chaining
         public static void Main(string[] args)
         {
             Random rand = new Random();
-            HashLinkList<int, int> hashCars = new HashLinkList<int, int>(2048);
-            for (int i = 0; i < 5120; i++)
+            HashLinkList<int, int> hashCars = new HashLinkList<int, int>(600);
+            for (int i = 0; i < 512; i++)
             {
-                hashCars.Add(rand.Next(1111111, 9999999), rand.Next(1930, 2020));
+                try
+                {
+                    hashCars.Add(rand.Next(1111111, 9999999), rand.Next(1930, 2020));
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                }
             }
 
+            //try to add the same key
+            try
+            {
+                hashCars.Add(6554736, rand.Next(1930, 2020));
+                hashCars.Add(6554736, rand.Next(1930, 2020));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+
+            //
             Console.WriteLine();
         }
     }
