64b37fcf34d31b2d4ae1b10e5ce7a4f32eff8f03
yanivn2000 <yanivn2000@gmail.com>
2020-08-10 12:55:04 +0300

    has3

--- Hash_SeparateChaining/Main.cs
+++ Hash_SeparateChaining/Main.cs
@@ -8,13 +8,11 @@ namespace Hash_SeparateChaining
         {
 
             Random rand = new Random();
-            Hash_SeparateChaining<int, int> hashCars = new Hash_SeparateChaining<int,int>();
+            Hash_SeparateChaining<int, int> hashCars = new Hash_SeparateChaining<int,int>(2048);
             for (int i = 0; i < 5120; i++)
             {
                 hashCars.Add(rand.Next(1111111, 9999999), rand.Next(1930, 2020));
             }
-
-
             
             Console.WriteLine();
         }
