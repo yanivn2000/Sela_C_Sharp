a294bf453a534372919d6de373d0d04811857138
yanivn2000 <yanivn2000@gmail.com>
2020-10-15 15:42:42 +0300

    1026- 4

--- classes_calc/Program.cs
+++ classes_calc/Program.cs
@@ -6,21 +6,56 @@ namespace classes_calc
     {
         public static void Main(string[] args)
         {
+            /*
             Calc calc = new Calc();
             int num1 = 5, num2 = 8;
-            int result = calc.Add(num1, num2);
-            Console.WriteLine($"The result of {num1} + {num2} is {result}");
-
             Console.WriteLine("Bye bye..");
+
+            Console.WriteLine($"The result of {num1} + {num2} is {calc.Add(num1, num2)}");
+            Console.WriteLine($"The result of {num1} - {num2} is {calc.Sub(num1, num2)}");
+            Console.WriteLine($"The result of {num1} x {num2} is {calc.Mult(num1, num2)}");
+            Console.WriteLine($"The result of {num1} / {num2} is {calc.Div(num1, num2)}");
+
+            */
+
+            CalcPro calc = new CalcPro();
+            int num1 = 5, num2 = 8, num3 = 66;
+            Console.WriteLine($"Result after add {num1} is {calc.Add(num1)}");
+            Console.WriteLine($"Result after add {num2} is {calc.Add(num2)}");
+            Console.WriteLine($"Result after add {num3} is {calc.Add(num3)}");
+
+        }
+    }
+
+
+    class CalcPro
+    {
+        private float _result = 0;
+
+        public float Add(float x)
+        {
+            _result += x;
+            return _result;
         }
     }
 
     class Calc
     {
-        public int Add(int x, int y)
+        public float Add(float x, float y)
         {
             return x + y;
         }
-
+        public float Sub(float x, float y)
+        {
+            return x - y;
+        }
+        public float Mult(float x, float y)
+        {
+            return x * y;
+        }
+        public float Div(float x, float y)
+        {
+            return x / y;
+        }
     }
 }
