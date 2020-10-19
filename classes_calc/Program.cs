cf238f82adc0aa2b246fbc24a9ea56e63bd7d06c
yanivn2000 <yanivn2000@gmail.com>
2020-10-19 10:09:54 +0300

    1026 - 19 Oct 1

--- classes_calc/Program.cs
+++ classes_calc/Program.cs
@@ -8,32 +8,41 @@ namespace classes_calc
         {
             /*
             Calc calc = new Calc();
-            int num1 = 5, num2 = 8;
+            int num1 = 5, num2 = 8, per = 30;
             Console.WriteLine("Bye bye..");
 
             Console.WriteLine($"The result of {num1} + {num2} is {calc.Add(num1, num2)}");
             Console.WriteLine($"The result of {num1} - {num2} is {calc.Sub(num1, num2)}");
             Console.WriteLine($"The result of {num1} x {num2} is {calc.Mult(num1, num2)}");
             Console.WriteLine($"The result of {num1} / {num2} is {calc.Div(num1, num2)}");
-
+            Console.WriteLine($"The result of {per}% of {num2} is {calc.Percent(num2, per)}");
             */
 
+            
             CalcPro calc = new CalcPro();
             float num1 = 5, num2 = 8, num3 = 66;
             Console.WriteLine($"Result after sub {num1} is {calc.Sub(num1)}");
+            Console.WriteLine($"Result after percent 20 is {calc.Percent(20)}");
             Console.WriteLine($"Result after add {num2} is {calc.Add(num2)}");
             Console.WriteLine($"Result after Reset is {calc.Reset()}");
             Console.WriteLine($"Result after add {num1} is {calc.Add(num1)}");
             Console.WriteLine($"Result after mult {num3} is {calc.Mult(num3)}");
             Console.WriteLine($"Result after div {num2} is {calc.Div(num2)}");
-
+            
         }
     }
 
 
     class CalcPro
     {
-        private float _result = 0;
+        public float _result = 0;
+
+        public float Percent(float x)
+        {
+            _result *= x/100;
+            //_result = _result * x / 100;
+            return _result;
+        }
 
         public float Add(float x)
         {
@@ -79,6 +88,10 @@ namespace classes_calc
         public float Div(float x, float y)
         {
             return x / y;
+        }
+        public float Percent(float val, float percent)
+        {
+            return val * percent / 100;
         }
     }
 }
