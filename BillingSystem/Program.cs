520ddfe840ec12df7824bcc03ae53ed253f6e9f3
yanivn2000 <yanivn2000@gmail.com>
2020-11-12 16:29:29 +0200

    1026 - 2 - Customers

---
+++ BillingSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BillingSystem
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our system billing ver 2");
+
+            Customer[] customers = new Customer[3];
+            for (int i = 0; i < customers.Length; i++)
+            {
+                Console.Write("What your name? ");
+                string name = Console.ReadLine();
+                customers[i] = new Customer(name, 0);
+            }
+
+
+            foreach (var customer in customers)
+            {
+                customer.PrintMe();
+            }
+        }
+    }
+
+}
