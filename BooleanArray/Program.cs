6e986ecb1797af7f9f37cf2de91f2ead4da5b77d
yanivn2000 <yanivn2000@gmail.com>
2020-11-16 12:35:31 +0200

    Set with bool array - 1026

---
+++ BooleanArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SstNumberBoolArray
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Set set1 = new Set();
+
+            Console.WriteLine("Print set1:");
+            foreach (var item in set1.GetSet())
+            {
+                Console.WriteLine(item);
+            }
+            int[] array = {5, 7, 20 , 33, 23};
+            Set set2 = new Set(array);
+            Console.WriteLine("Print set2:");
+            foreach (var item in set2.GetSet())
+            {
+                Console.WriteLine(item);
+            }
+            set1.Insert(10);
+            set1.Insert(20);
+            set1.Insert(30);
+            Console.WriteLine("Print set1:");
+            foreach (var item in set1.GetSet())
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ BooleanArray/Set.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace SstNumberBoolArray
+{
+    public class Set
+    {
+        private int _set_counter;
+        private bool[] _array;
+        private int _size = 100;
+
+        public Set()
+        {
+            _array = new bool[_size];
+            _set_counter = 0;
+        }
+        public Set(int [] arrayInt)//arrayInt[0] = 66, array[1] = 5
+        {
+            _array = new bool[_size];
+            foreach (int item in arrayInt)
+            {
+                if (IsInRange(item))
+                {
+                    _set_counter++;
+                    _array[item] = true;
+                }
+            }
+        }
+        private bool IsInRange(int number)
+        {
+            return number >= 0 && number <= _size;
+        }
+        public bool IsExist(int number)
+        {
+            return _array[number];
+        }
+        public bool Insert(int number)
+        {
+            if (IsInRange(number)) {
+                if (_array[number] == false) {
+                    _set_counter++;
+                    _array[number] = true;
+                }
+            }
+            else
+                return false;
+
+            return true;
+        }
+        public bool Remove(int number)
+        {
+            if (IsInRange(number)){
+                if (_array[number])
+                {
+                    _set_counter--;
+                    _array[number] = false;
+                }
+            }
+            else
+                return false;
+            return true;
+        }
+        public int[] GetSet()
+        {
+            int[] arrayInt = new int[_set_counter];
+            
+            for (int j = 0, i = 0; i < _size; i++)
+            {
+                if (_array[i] == true)
+                    arrayInt[j++] = i;
+            }
+            return arrayInt;
+        }
+    }
+}
